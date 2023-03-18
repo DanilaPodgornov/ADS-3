99702ddb0ef5f9e69807ea0c109b88b78de10982
DanilaPodgornov <116427593+DanilaPodgornov@users.noreply.github.com>
2023-03-18 18:50:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,25 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    if (size > 0) {
+        int middleIndex = size / 2;
+        if (value == arr[middleIndex]) {
+            int count = 0;
+            int border = middleIndex - 1;
+            while (value == arr[middleIndex]) {
+                count++;
+                middleIndex++;
+            }
+            while (value == arr[border]) {
+                count++;
+                border--;
+            }
+            return count;
+        } else if (value < arr[middleIndex]) {
+            cbinsearch(arr, middleIndex, value);
+        } else {
+            cbinsearch(arr + middleIndex + 1, size - middleIndex - 1, value);
+        }
+    } else
+    return 0;
 }
